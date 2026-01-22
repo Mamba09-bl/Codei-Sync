1c6a73e9fc2104b693e286918aee73e7faf82372
unknown <hamza.ahmed.abbasi07@gmail.com>
2026-01-22 05:20:28 +0500

    Update project files

--- app/showCode/[id]/[name]/page.js
+++ app/showCode/[id]/[name]/page.js
@@ -1,7 +1,7 @@
 "use client";
 import { useParams } from "next/navigation";
 import { useEffect, useState } from "react";
-// import { socket } from "@/lib/clientSocket";
+// import { socket } from "@/lib/clientSocket s";
 import socket from "@/lib/clientSocket";
 
 import Editor from "@monaco-editor/react";
